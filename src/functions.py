d79c639d6ee4844a2661af760b13cb77a9a57c8e
Jacob Prebys <jakers991@gmail.com>
2020-09-03 16:19:36 -0500

    pivoting

--- src/functions.py
+++ src/functions.py
@@ -5,3 +5,12 @@ def fao_global_averages(data, food):
     '''
     prices = data[data.Item == food]
     return prices.groupby('Year').Value.mean()    
+
+def fao_pivot(data, food):
+    '''
+    take the FAO data and the name of a food
+    return the pivot table where the rows are the countries,
+    the columns are the years, and the values are the values
+    '''
+    foods = data[data.Item == food]
+    return foods.pivot(index='Area', columns='Year', values='Value')
